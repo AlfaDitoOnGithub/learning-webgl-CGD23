f417fb1d940bde6726cdbd7b354603987aa43c4f
AlfaDitoOnGithub <alfaditoauliadenova@gmail.com>
2023-10-05 07:35:23 +0700

    Initial commit

---
+++ position.js
@@ -0,0 +1,143 @@
+// positions.js
+console.log("Exporting trianglePositions:", trianglePositions);
+console.log("Exporting linePositions:", linePositions);
+export const trianglePositions = [
+    // Define your triangle positions here
+    // ...
+    // Define the vertices for the letter 'A' using triangles and lines
+        //Letter A
+        // Triangle 1
+        -0.79, -0.6,
+        -0.78, 0.6,
+        -0.7, 0.6,
+        
+        // Triangle 2
+        -0.7, 0.6,
+        -0.74, -0.6,
+        -0.79, -0.6,
+
+        // triangle 3 konektor tengah1
+        -0.55, 0.1,
+        -0.75, 0.0,
+        -0.55, -0.0,
+
+        //triangle 4 konektor tengah2
+        -0.55, 0.1,
+        -0.75, 0.0,
+        -0.75, 0.1,
+
+        //triangle 5
+        -0.49, -0.6,
+        -0.59, 0.6,
+        -0.49, 0.6,
+        
+        // Triangle 6
+        -0.59, 0.6,
+        -0.49, -0.6,
+        -0.53, -0.6,
+
+        // triangle 7 -konektor atas
+        // triangle 3 konektor tengah1
+        -0.55, 0.6,
+        -0.75, 0.5,
+        -0.55, 0.5,
+
+        //triangle 4 konektor tengah2
+        -0.55, 0.6,
+        -0.75, 0.5,
+        -0.75, 0.6,
+
+        //letter L
+        //vert1
+        -0.4, 0.6,
+        -0.4, -0.6,
+        -0.3, 0.6,
+
+        //vert2
+        -0.3, -0.6,
+        -0.4, -0.6,
+        -0.3, 0.6,
+
+        //hort1
+        -0.3, -0.5,
+        -0.3, -0.6,
+        0.0, -0.5,
+
+        //hort2
+        -0.3, -0.6,
+        0.0, -0.6,
+        0.0, -0.5,
+
+        //letter F
+        //vert1
+        0.05, 0.6,
+        0.05, -0.6,
+        0.15, 0.6,
+
+        //vert2
+        0.15, -0.6,
+        0.05, -0.6,
+        0.15, 0.6,
+
+        // toppart 1
+        0.15, 0.6,
+        0.1, 0.5,
+        0.45, 0.5,
+
+        // toppart 1
+        0.15, 0.6,
+        0.45, 0.6,
+        0.45, 0.5,
+
+        // midpart1
+        0.15, 0.2,
+        0.1, 0.1,
+        0.45, 0.1,
+
+        // midpart2
+        0.15, 0.2,
+        0.45, 0.2,
+        0.45, 0.1,
+        
+    
+
+
+    
+];
+
+export const linePositions = [
+    // Define your line positions here
+    // ...
+        // Lines for 'A'
+         
+        -0.6, -0.4,
+         -0.4, 0.5,
+         
+         -0.2, -0.4,
+         -0.4, 0.5,
+         
+         -0.55, -0.0,
+         -0.25, -0.0,
+         
+         // Add lines for other characters...
+         // lines of L
+         -0.1, -0.4,
+         -0.1, 0.5,
+         
+         -0.1, -0.4,
+         0.2, -0.4,
+         
+         //lines of f
+         0.3, 0.5,
+         0.6, 0.5,
+ 
+         0.3, 0.1,
+         0.6, 0.1,
+ 
+         0.3, 0.5,
+         0.3, -0.4,
+ 
+ 
+];
+
+
